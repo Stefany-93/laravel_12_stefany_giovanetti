93def0f1d8a275a86c7dd2d1005d60afdf2fc3c2
Stefany Giovanetti <ste_giovanetti@yahoo.it>
2026-03-14 10:53:26 +0100

    Cominciato esercizio

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -31,18 +31,29 @@ class PublicController extends Controller
                 return view('crea');
         }
 
-        public function creaPanino(PaninoRequest $request) {
+        // public function creaPanino(PaninoRequest $request) {
 
-                $panino = new Panino();
-                $panino->name = $request->name;
-                $panino->description = $request->description;
-                if ($request->hasFile('img')) {
-                        $panino->img = $request->file('img')->store('images', 'public');
-                } else {
-                        $panino->img = 'images/no-img.png';
-                }
-                $panino->save();
+        //         $panino = new Panino();
+        //         $panino->name = $request->name;
+        //         $panino->description = $request->description;
+        //         if ($request->hasFile('img')) {
+        //                 $panino->img = $request->file('img')->store('images', 'public');
+        //         } else {
+        //                 $panino->img = 'images/no-img.png';
+        //         }
+        //         $panino->save();
 
+        //         return redirect()->route('panini.index');
+        // }
+        public function creaPanino(PaninoRequest $request){
+                $imgPath = $request->hasFile('img')
+                ? $request->file('img')->store('images', 'public')
+                : 'images/no-img.png';
+                Panino::create([
+                        'name' => $request->name,
+                        'description' => $request->description,
+                        'img' => $imgPath
+                ]);
                 return redirect()->route('panini.index');
         }
 
